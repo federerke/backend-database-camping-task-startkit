a8d1f5991a7ca07598195dfd7f6fc3bbeb167b37
彥廷 柯 <yspm.ke@gmail.com>
2024-12-08 19:38:05 +0800

    修改錯誤

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -64,14 +64,14 @@ VALUES(
 INSERT INTO "CREDIT_PURCHASE"(user_id,credit_package_id,purchased_credits,price_paid)
 VALUES(
 	(SELECT id FROM "USER" WHERE email='wXlTq@hexschooltest.io'),
-    (SELECT id FROM "CREDIT_PACKAGE" WHERE WHERE = '21 堂組合包方案'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE  = '21 堂組合包方案'),
 	(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
 	(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案')
 );
 INSERT INTO "CREDIT_PURCHASE"(user_id,credit_package_id,purchased_credits,price_paid)
 VALUES(
 	(SELECT id FROM "USER" WHERE email='richman@hexschooltest.io'),
-    (SELECT id FROM "CREDIT_PACKAGE" WHERE WHERE = '14 堂組合包方案'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE  = '14 堂組合包方案'),
 	(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
 	(SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案')
 );
